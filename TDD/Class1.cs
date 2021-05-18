2001b8a0842d24b35b624a21c9dd3808ef1bf460
Przemysław Jędral <jedral.przemek@gmail.com>
2021-05-18 20:30:49 +0200

    commit_2

--- TDD/Class1.cs
+++ TDD/Class1.cs
@@ -6,8 +6,17 @@ namespace TDD
 {
     public class Class1
     {
-        public static int Choose_max(int a, int b) {
+        public static int Choose_max(int a, int b)
+        {
             if (a > b)
+                return a;
+            else
+                return b;
+        }
+
+        public static int Choose_min(int a, int b)
+        {
+            if (a < b)
                 return a;
             else
                 return b;
