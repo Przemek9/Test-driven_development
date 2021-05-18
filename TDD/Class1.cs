1c5786c76b3dff325a063f3486074505bf17338b
KWerem <74089244+KWerem@users.noreply.github.com>
2021-05-18 15:43:46 +0200

    Update Class1.cs

--- TDD/Class1.cs
+++ TDD/Class1.cs
@@ -6,7 +6,12 @@ namespace TDD
 {
     public class Class1
     {
-
+        public static int Choose_max(int a, int b) {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
 
     }
 }
